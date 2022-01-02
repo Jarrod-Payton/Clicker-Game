5c5b8e59d4100bb24cd6a04f46bc7df00772ef99
Jarrod-Payton <lucasjpayton101@gmail.com>
2022-01-01 16:18:38 -0800

    hesslo

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <button @click="Tap"></button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { AppState } from "../AppState";
+export default {
+  name: "Home",
+  setup() {
+    const points = computed(() => AppState.points);
+    return {
+      tap() {},
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
